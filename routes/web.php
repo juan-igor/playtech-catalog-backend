016de24314273a283ed28f28f185ee684e54b2fd
Juan Igor <juan.igorbdf@gmail.com>
2020-08-10 13:40:50 -0300

    Changed main web route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'application' => 'Playtech Solutions - Catálogo de Vendas',
+        'version' => '0.1',
+    ]);
 });
+
+// Auth::routes(['verify' => true]);
