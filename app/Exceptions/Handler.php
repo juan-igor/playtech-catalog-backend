9a3031ed9ced36a19058c01c0c1ecd7c55b8b38a
Juan Igor <juan.igorbdf@gmail.com>
2020-08-10 13:38:12 -0300

    Updated laravel exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,14 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\Response;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -13,7 +21,11 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Session\TokenMismatchException::class,
+        \Illuminate\Validation\ValidationException::class,
     ];
 
     /**
@@ -32,7 +44,7 @@ class Handler extends ExceptionHandler
      * @param  \Throwable  $exception
      * @return void
      *
-     * @throws \Exception
+     * @throws \Throwable
      */
     public function report(Throwable $exception)
     {
@@ -44,12 +56,104 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
+     * @return \Illuminate\Http\Response
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof UnauthorizedHttpException) {
+            $preException = $exception->getPrevious();
+            if ($preException instanceof TokenExpiredException) {
+                return response()->json([
+                    'success' => false,
+                    'error' => [
+                        'message' => 'TOKEN_EXPIRED',
+                    ],
+                ], 401);
+            } elseif ($preException instanceof TokenInvalidException) {
+                return response()->json([
+                    'success' => false,
+                    'error' => [
+                        'message' => 'TOKEN_INVALID',
+                    ],
+                ]);
+            } elseif ($preException instanceof TokenBlacklistedException) {
+                return response()->json([
+                    'success' => false,
+                    'error' => [
+                        'message' => 'TOKEN_BLACKLISTED',
+                    ],
+                ], 401);
+            }
+        }
+
+        if ($exception->getMessage() === 'Token not provided') {
+            return response()->json([
+                'success' => false,
+                'error' => [
+                    'message' => 'Token not provided',
+                ],
+            ], 401);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->json([
+                'success' => false,
+                'error' => [
+                    'message' => 'Unauthenticated',
+                ],
+            ], 401);
+        }
+
+        return $this->prepareJsonResponse($request, $exception);
+    }
+
+    protected function prepareJsonResponse($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return response()->json([
+                'success' => false,
+                'error' => [
+                    'exception' => $this->getExceptionClass($exception),
+                    'message' => $exception->getMessageBag(),
+                ],
+            ], 400);
+        }
+
+        $exception = FlattenException::create($exception);
+
+        if (config('app.debug')) {
+            $message = $exception->getMessage();
+            if(strlen($message) === 0){
+                $message = Response::$statusTexts[$exception->getStatusCode()];
+            }
+        } else {
+            $message = Response::$statusTexts[$exception->getStatusCode()];
+        }
+
+        return response()->json([
+            'success' => false,
+
+            'error' => [
+                'exception' => $this->getExceptionClass($exception),
+                'http_code' => $exception->getStatusCode(),
+                'message' => $message,
+                'trace' => $this->getTrace($exception),
+            ],
+
+        ], $exception->getStatusCode());
+    }
+
+    private function getTrace($exception)
+    {
+        if (config('app.debug')) {
+            return 'file: '.$exception->getFile().' line: '.$exception->getLine();
+        }
+    }
+
+    private function getExceptionClass($exception)
+    {
+        if (config('app.debug')) {
+            return get_class($exception);
+        }
     }
 }
