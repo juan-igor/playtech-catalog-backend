899ea7bd1497f41e05aa4d51774ba1954f06d2e2
Juan Igor <juan.igorbdf@gmail.com>
2020-09-04 11:40:20 -0300

    Added auth and dashboard controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['welcome']]);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('dashboard');
+    }
+
+    public function welcome()
+    {
+        return view('welcome');
+    }
+}
